34ae687a025ee3c9ae368da6f7fcdd026f876b75
isucon <you@example.com>
2015-09-27 17:12:46 +0900

    fix friends

--- templates/friends.php
+++ templates/friends.php
@@ -1,9 +1,9 @@
 <h2>友だちリスト</h2>
 <div class="row panel panel-primary" id="friends">
   <dl>
-    <?php foreach ($friends as $friend) { ?>
+    <?php foreach ($friends as $user_id => $created_at) { ?>
     <?php $friend_info = get_user($user_id) ?>
-    <dt class="friend-date"><?php h($friend['created_at']) ?></dt><dd class="friend-friend"><a href="/profile/<?php h($friend_info['account_name']) ?>"><?php h($friend_info['nick_name']) ?></a></dd>
+    <dt class="friend-date"><?php h($created_at) ?></dt><dd class="friend-friend"><a href="/profile/<?php h($friend_info['account_name']) ?>"><?php h($friend_info['nick_name']) ?></a></dd>
     <?php } ?>
   </dl>
 </div>
